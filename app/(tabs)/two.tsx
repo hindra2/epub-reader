815ebc28a736e5782035dde060d3515816c4afa6
hindra2 <harold.indra@gmail.com>
2025-03-09 16:40:55 -0500

    removed boilerplate code

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,22 +1,13 @@
 import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
-
-import { ScreenContent } from '~/components/ScreenContent';
+import { Text, View } from 'react-native';
 
 export default function Home() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Tab Two' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/two.tsx" title="Tab Two" />
+      <Stack.Screen options={{ title: 'Tab One' }} />
+      <View>
+        <Text>Test 2</Text>
       </View>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
